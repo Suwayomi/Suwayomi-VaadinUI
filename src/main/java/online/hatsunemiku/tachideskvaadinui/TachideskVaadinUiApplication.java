fc367fb7b32f7e0ef0b55ebb54b0802580507685
Alessandro Schwaiger <aless2003@gmx.at>
2023-06-03 16:33:39 +0200

    init

---
+++ src/main/java/online/hatsunemiku/tachideskvaadinui/TachideskVaadinUiApplication.java
@@ -0,0 +1,13 @@
+package online.hatsunemiku.tachideskvaadinui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachideskVaadinUiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TachideskVaadinUiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/online/hatsunemiku/tachideskvaadinui/TachideskVaadinUiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.hatsunemiku.tachideskvaadinui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TachideskVaadinUiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
