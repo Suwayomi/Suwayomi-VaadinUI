508d6427716d61ede1ecc8d03c9aa9ae6562e29b
Alessandro Schwaiger <aless2003@gmx.at>
2023-08-21 17:07:00 +0200

    Refactor AniListScoreFormat for data efficiency
    Removed 'format' string from AniListScoreFormat enum to increase data efficiency. All references to 'format' have been deleted. The structure of the enum has been adjusted accordingly. This change simplifies the data structure, reducing redundancy.

--- src/main/java/online/hatsunemiku/tachideskvaadinui/data/tracking/anilist/AniListScoreFormat.java
+++ src/main/java/online/hatsunemiku/tachideskvaadinui/data/tracking/anilist/AniListScoreFormat.java
@@ -2,21 +2,19 @@ package online.hatsunemiku.tachideskvaadinui.data.tracking.anilist;
 
 import lombok.Getter;
 
+@Getter
 public enum AniListScoreFormat {
-  POINT_100("POINT_100", 100, 1),
-  POINT_10_DECIMAL("POINT_10_DECIMAL", 10, 1),
-  POINT_10("POINT_10", 10, 1),
-  POINT_5("POINT_5", 5, 1),
-  POINT_3("POINT_3", 3, 1);
+  POINT_100(100, 1),
+  POINT_10_DECIMAL(10, 1),
+  POINT_10(10, 1),
+  POINT_5(5, 1),
+  POINT_3(3, 1);
 
-  private final String format;
-  @Getter
   private final int maxScore;
   @Getter
   private final int minScore;
 
-  AniListScoreFormat(String format, int maxScore, int minScore) {
-    this.format = format;
+  AniListScoreFormat(int maxScore, int minScore) {
     this.maxScore = maxScore;
     this.minScore = minScore;
   }
