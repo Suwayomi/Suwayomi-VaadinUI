4bed63f35582532bb1e9b8ab07798d687372fdf2
Alessandro Schwaiger <aless2003@gmx.at>
2023-08-26 18:55:54 +0200

    Remove unused constructor in SourceFilterData
    The default constructor of SourceFilterData which received only a searchTerm was removed since it wasn't being used.

--- src/main/java/online/hatsunemiku/tachideskvaadinui/data/tachidesk/search/SourceFilterData.java
+++ src/main/java/online/hatsunemiku/tachideskvaadinui/data/tachidesk/search/SourceFilterData.java
@@ -3,7 +3,5 @@ package online.hatsunemiku.tachideskvaadinui.data.tachidesk.search;
 import java.util.List;
 
 public record SourceFilterData(String searchTerm, List<SourceFilterChange> filter) {
-  public SourceFilterData(String searchTerm) {
-    this(searchTerm, List.of());
-  }
+
 }
