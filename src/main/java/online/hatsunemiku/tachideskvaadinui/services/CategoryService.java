b04ca5c4fc305c7eec77a374d0992c97fc9ad9da
Alessandro Schwaiger <aless2003@gmx.at>
2023-10-08 15:40:25 +0200

    Replaced CategoryClient with a new GraphQL variant

--- src/main/java/online/hatsunemiku/tachideskvaadinui/services/CategoryService.java
+++ src/main/java/online/hatsunemiku/tachideskvaadinui/services/CategoryService.java
@@ -6,9 +6,7 @@
 
 package online.hatsunemiku.tachideskvaadinui.services;
 
-import java.net.URI;
 import java.util.List;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import online.hatsunemiku.tachideskvaadinui.data.tachidesk.Category;
 import online.hatsunemiku.tachideskvaadinui.data.tachidesk.Manga;
@@ -16,24 +14,22 @@ import online.hatsunemiku.tachideskvaadinui.services.client.CategoryClient;
 import org.jetbrains.annotations.Contract;
 import org.springframework.stereotype.Service;
 
-/** This class provides methods for interacting with the Tachidesk Category API. */
+/**
+ * This class provides methods for interacting with the Tachidesk Category API.
+ */
 @Slf4j
 @Service
 public class CategoryService {
-
   private final CategoryClient categoryClient;
-  private final SettingsService settingsService;
 
   /**
-   * Constructs a new CategoryService object.
+   * Constructs a new instance of {@link CategoryService} with the specified {@link CategoryClient}.
    *
-   * @param categoryClient the client used for interacting with the category API
-   * @param settingsService the service used for retrieving settings
+   * @param categoryClient the client used to perform operations related to categories
    */
   @Contract(pure = true)
-  public CategoryService(CategoryClient categoryClient, SettingsService settingsService) {
+  public CategoryService(CategoryClient categoryClient) {
     this.categoryClient = categoryClient;
-    this.settingsService = settingsService;
   }
 
   /**
@@ -43,15 +39,7 @@ public class CategoryService {
    * @return true if the category was created successfully, false otherwise
    */
   public boolean createCategory(String name) {
-    URI baseUrl = URI.create(settingsService.getSettings().getUrl());
-
-    try {
-      categoryClient.createCategory(baseUrl, Map.of("name", name));
-      return true;
-    } catch (Exception e) {
-      log.error("Failed to create category", e);
-      return false;
-    }
+    return categoryClient.createCategory(name);
   }
 
   /**
@@ -61,43 +49,28 @@ public class CategoryService {
    * @return true if the category was deleted successfully, false otherwise
    */
   public boolean deleteCategory(int categoryId) {
-    URI baseUrl = URI.create(settingsService.getSettings().getUrl());
-
-    try {
-      categoryClient.deleteCategory(baseUrl, categoryId);
-      return true;
-    } catch (Exception e) {
-      log.error("Failed to delete category", e);
-      return false;
-    }
+    return categoryClient.deleteCategory(categoryId);
   }
 
   /**
    * Retrieves a list of categories from the server.
    *
    * @return a {@link List} of {@link Category} objects representing the categories retrieved from
-   *     the server
+   * the server
    * @throws RuntimeException if an error occurs while retrieving the categories
    */
   public List<Category> getCategories() {
-    URI baseUrl = URI.create(settingsService.getSettings().getUrl());
-
-    try {
-      return categoryClient.getCategories(baseUrl);
-    } catch (Exception e) {
-      log.debug("Failed to get categories", e);
-      throw new RuntimeException(e);
-    }
+    return categoryClient.getCategories();
   }
 
+  /**
+   * Retrieves a list of manga belonging to a specific category.
+   *
+   * @param categoryId the ID of the category to retrieve manga from
+   * @return a {@link List} of {@link Manga} objects representing the manga retrieved from the
+   * specified category
+   */
   public List<Manga> getMangaFromCategory(int categoryId) {
-    URI baseUrl = URI.create(settingsService.getSettings().getUrl());
-
-    try {
-      return categoryClient.getMangaFromCategory(baseUrl, categoryId);
-    } catch (Exception e) {
-      log.error("Failed to get manga from category", e);
-      throw new RuntimeException(e);
-    }
+    return categoryClient.getCategoryManga(categoryId);
   }
 }
