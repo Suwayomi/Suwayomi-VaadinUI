0aafb8d6b67b656501010f8f3aeeddd5470d2a34
Alessandro Schwaiger <aless2003@gmx.at>
2024-06-10 21:01:13 +0200

    Fix JAVA-D1000

--- src/main/java/online/hatsunemiku/tachideskvaadinui/data/settings/FlareSolverrSettings.java
+++ src/main/java/online/hatsunemiku/tachideskvaadinui/data/settings/FlareSolverrSettings.java
@@ -9,17 +9,37 @@ package online.hatsunemiku.tachideskvaadinui.data.settings;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+/**
+ * This class represents the settings used for FlareSolverr}.
+ *
+ * @see <a href="https://github.com/FlareSolverr/FlareSolverr">FlareSolverr</a>
+ */
 @Data
 public class FlareSolverrSettings {
 
+  /**
+   * A boolean indicating whether FlareSolverr is enabled on the server.
+   */
   @JsonProperty("flareSolverrEnabled")
   private boolean enabled;
+  /**
+   * The name of the FlareSolverr session.
+   */
   @JsonProperty("flareSolverrSessionName")
   private String sessionName;
+  /**
+   * The time-to-live (TTL) of the FlareSolverr session.
+   */
   @JsonProperty("flareSolverrSessionTtl")
   private int sessionTTL;
+  /**
+   * The timeout for the FlareSolverr session, in seconds.
+   */
   @JsonProperty("flareSolverrTimeout")
   private int timeout;
+  /**
+   * The URL of the FlareSolverr server. e.g. http://localhost:8191
+   */
   @JsonProperty("flareSolverrUrl")
   private String url;
 }
