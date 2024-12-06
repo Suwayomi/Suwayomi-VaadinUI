d9c5fa5907a80c5dacb401f1fef27f47eb199c6f
Alessandro Schwaiger <aless2003@gmx.at>
2024-12-06 21:15:45 +0100

    Open Browser if an instance is already running

--- src/main/java/online/hatsunemiku/tachideskvaadinui/TachideskVaadinUiApplication.java
+++ src/main/java/online/hatsunemiku/tachideskvaadinui/TachideskVaadinUiApplication.java
@@ -6,7 +6,10 @@
 
 package online.hatsunemiku.tachideskvaadinui;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import lombok.extern.slf4j.Slf4j;
+import online.hatsunemiku.tachideskvaadinui.utils.BrowserUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -34,11 +37,38 @@ public class TachideskVaadinUiApplication {
   public static void main(String[] args) {
     boolean headless = Boolean.parseBoolean(System.getProperty("vaaui.headless"));
 
+    if (isRunningAlready()) {
+      log.warn("Application is already running.");
+      runAlreadyRunningTasks(headless);
+      System.exit(0);
+    }
+
     log.info("Is headless: {}", headless);
 
     SpringApplication app =
         new SpringApplicationBuilder(TachideskVaadinUiApplication.class).headless(headless).build();
 
     app.run(args);
+  }
+
+  private static void runAlreadyRunningTasks(boolean headless) {
+    if (!headless) {
+      log.info("Opening browser...");
+
+      try {
+        BrowserUtils.openBrowser("http://localhost:3901");
+      } catch (IOException e) {
+        log.error("Failed to open browser.", e);
+      }
+    }
+  }
+
+  private static boolean isRunningAlready() {
+    int port = 3901;
+    try (ServerSocket ignored = new ServerSocket(port)) {
+      return false;
+    } catch (IOException e) {
+      return true;
+    }
   }
 }
